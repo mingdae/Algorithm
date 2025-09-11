7ab42a2da14a7e8260e970886fa8bb59dbad21f4
mingdae <103407209+mingdae@users.noreply.github.com>
2025-09-11 20:25:02 +0900

    [Silver IV] Title: 동전 0, Time: 104 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+/* [문제 분석]
+ *  - N종류 동전
+ *  - 가치의 합 K, 필요한 동전 최소값
+ * 
+ */
+
+public class Main {
+	static int n, total, ans;
+	static int[] coins;
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		n = Integer.parseInt(st.nextToken());
+		total = Integer.parseInt(st.nextToken());
+		ans = 0;
+		coins = new int[n];
+		
+		for(int i = 0 ; i < n ; i++) {
+			coins[i] = Integer.parseInt(br.readLine());
+		}
+		
+		for(int i = n-1; i >= 0; i--) {
+			ans += total / coins[i];
+			total %= coins[i];
+			if(total == 0)
+				break;
+		}
+		
+		System.out.println(ans);
+	}
+}
+	
+	
