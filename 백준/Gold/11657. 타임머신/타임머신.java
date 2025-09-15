941dd69ec05166e5a079a4f0b75613ae0162f619
mingdae <103407209+mingdae@users.noreply.github.com>
2025-09-15 23:09:58 +0900

    [Gold IV] Title: 타임머신, Time: 252 ms, Memory: 19000 KB -BaekjoonHub

--- 백준/Gold/11657. 타임머신/타임머신.java
+++ 백준/Gold/11657. 타임머신/타임머신.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.util.*;
 
+
 public class Main {
 	
 	static class Edge{
@@ -14,21 +15,23 @@ public class Main {
 		
 	}
 	
+	
 	static int V, E;
 	static ArrayList<Edge> edgeList;
 	static long[] minDist;
 	static final long INF = Long.MAX_VALUE / 4;
 	
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		StringBuilder sb = new StringBuilder();
 		
 		V = Integer.parseInt(st.nextToken());
 		E = Integer.parseInt(st.nextToken());
-		edgeList = new ArrayList<>();
+		
 		minDist = new long[V+1];
 		Arrays.fill(minDist, INF);
+		edgeList = new ArrayList<>();
 		
 		for(int i = 0 ; i < E; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -37,17 +40,17 @@ public class Main {
 			int weight = Integer.parseInt(st.nextToken());
 			edgeList.add(new Edge(from, to, weight));
 		}
-		boolean noCycle = bellmanFord(1);
 		
-		if(!noCycle)	System.out.println(-1);
-		else {
+		if(bellmanFord(1)) { // 사이클 X
 			for(int i = 2; i <= V; i++) {
-				if(minDist[i] == INF)	sb.append(-1);
-				else sb.append(minDist[i]);
-				
-				if(i != V)	sb.append('\n');
+				if(minDist[i] == INF)
+					sb.append(-1).append('\n');
+				else
+					sb.append(minDist[i]).append('\n');
 			}
 			System.out.println(sb);
+		}else {
+			System.out.println(-1);
 		}
 	}
 	
@@ -57,30 +60,34 @@ public class Main {
 		// V-1 라운드 진행
 		for(int i = 1; i < V; i++) {
 			boolean updated = false;
-			// 모든 간선 순회
+			
 			for(Edge e : edgeList) {
-				// 미도달 정점에서 출발하는 간선 스킵
+				// 미도달 정점 패스
 				if(minDist[e.from] == INF)	continue;
 				
-				 long nextDist = minDist[e.from] + e.weight;
-				 if(minDist[e.to] > nextDist) {
-					 minDist[e.to] = nextDist;
-					 updated = true;
-				 }
+				// 갱신
+				if(minDist[e.to] > minDist[e.from] + e.weight) {
+					minDist[e.to] = minDist[e.from] + e.weight;
+					updated = true;
+				}
 			}
 			
-			// update 안되면 바로 종료
-			if(!updated) break;
+			// 갱신 없으면 종료
+			if(!updated)	break;
 		}
 		
-		// 음수 사이클 검사 ( 시작점에서 도달 가능한 간선만)
+		// 음수 사이클 판별
 		for(Edge e : edgeList) {
+			// 미도달 정점 패스
 			if(minDist[e.from] == INF)	continue;
-			// 한 번 더 완화되면 음수 사이클 존재
-			if(minDist[e.from] + e.weight < minDist[e.to])
+			
+			if(minDist[e.to] > minDist[e.from] + e.weight) {
 				return false;
+			}
 		}
 		
-		return true;
+		return true; // 사이클 X
 	}
 }
+	
+	
