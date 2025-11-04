5bc1033c3062c2344ab5fba2f83cc2f8d268e581
mingdae <103407209+mingdae@users.noreply.github.com>
2025-11-04 11:29:25 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -0,0 +1,11 @@
+-- 입양을 간 기록은 있는데, 보호소에 들어온 기록이 없는 동물의 ID와 이름을 ID 순으로
+
+
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_OUTS AS AO 
+WHERE ANIMAL_ID NOT IN (
+    SELECT ANIMAL_ID
+    FROM ANIMAL_INS
+)
+ORDER BY ANIMAL_ID
+
